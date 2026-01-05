35663630a9179bcdee86b930018f0a722b4aad17
Mark <suguitanmark123@gmail.com>
2026-01-05 23:10:47 +0800

    first commit

---
+++ pos-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home, Auth, Orders } from './pages'
+import Header from './components/shared/Header';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ pos-frontend/src/components/home/PopularDishes.jsx
@@ -0,0 +1,34 @@
+import { popularDishes } from '../../constants';
+
+const PopularDishes = () => {
+  return (
+    <div className='mt-6 pr-6'>
+        <div className='bg-[#1a1a1a] w-full rounded-lg'>
+            <div className="flex justify-between items-center px-6 py-4">
+                <h1 className="text-white text-lg font-semibold tracking-wide">Popular Dishes</h1>
+                <a href="" className="text-[#025cca] text-sm font-semibold">View All</a>
+            </div>
+
+            <div className='overflow-y-scroll h-[680px]'>
+                {
+                    popularDishes.map((dish) => (
+                        <div key={dish.id} className='flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-6 py-4 mx-6 mt-4' >
+                            <h1 className='text-white font-bold text-xl mr-4'> { dish.id < 10 ? `0${dish.id}` : dish.id } </h1>
+                            <img src={dish.image} alt={dish.name} className='w-[50px] h-[50px] rounded-full' />
+                            <div>
+                                <h1 className='text-white font-semibold tracking-wide'>{dish.name}</h1>
+                                <p className='text-gray-400 text-sm font-semibold mt-1'>
+                                    <span className='text-[#ababab]'>Orders: </span> 
+                                    {dish.numberOfOrders}
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PopularDishes
